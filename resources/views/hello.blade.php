4dd683ea1e01b4fc3a3438e0021f92f901f975ac
takaaki <takaaking26@gmail.com>
2023-05-09 00:22:00 +0900

    Initial commit

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>hello world</title>
+</head>
+<body>
+    <h1>Hello World!!</h1>
+    <p style="color: red">こんにちは{{$name}}さん</p>
+    <p style="color: red">本コースでは{{$course}}を勉強します</p>
+</body>
+</html>
---
+++ resources/views/hello_world.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>hello world</title>
+</head>
+<body>
+    <h1>Hello World!!</h1>
+    <p style="color: red">こんにちは</p>
+</body>
+</html>
